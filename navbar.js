4cebaff9ca02f0f9fc41ef36bcaf7a2f14a2e8dc
iamkp19 <iamkp19.19@gmail.com>
2021-12-19 00:46:56 +0530

    Format JS File

--- navbar.js
+++ navbar.js
@@ -4,10 +4,3 @@ const navlinks = document.getElementsByClassName("navlinks")[0];
 togglebtn.addEventListener("click", () => {
   navlinks.classList.toggle("active");
 });
-
-// const toggleButton = document.getElementsByClassName('toggle-button')[0]
-// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-// toggleButton.addEventListener('click', () => {
-//   navbarLinks.classList.toggle('active')
-// })
